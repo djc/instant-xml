6455801950535fc6c476d9d6057cd70d2bd637c9
rsdy <p@symmetree.dev>
2022-09-26 16:52:29 +0100

    Add integration test for rename_all attribute

--- instant-xml/tests/rename.rs
+++ instant-xml/tests/rename.rs
@@ -21,3 +21,23 @@ fn renamed() {
         "<renamed renamed=\"true\"></renamed>"
     );
 }
+
+#[test]
+fn rename_all_struct() {
+    #[derive(Debug, PartialEq, Eq, ToXml, FromXml)]
+    #[xml(rename_all = "UPPERCASE")]
+    pub struct TestStruct {
+        field_1: String,
+        #[xml(attribute)]
+        field_2: bool,
+    }
+
+    let serialized = r#"<TestStruct FIELD_2="true"><FIELD_1>value</FIELD_1></TestStruct>"#;
+    let instance = TestStruct {
+        field_1: "value".into(),
+        field_2: true,
+    };
+
+    assert_eq!(to_string(&instance).unwrap(), serialized);
+    assert_eq!(from_str::<TestStruct>(serialized), Ok(instance));
+}

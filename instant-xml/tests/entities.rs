eea00c7180e6df5690e2d641af7df5205d6b6435
rsdy <p@symmetree.dev>
2022-09-29 17:45:17 +0100

    Better serialization of complex types under `Vec<T>`

--- instant-xml/tests/entities.rs
+++ instant-xml/tests/entities.rs
@@ -13,6 +13,33 @@ struct StructSpecialEntities<'a> {
     vec: Vec<String>,
 }
 
+#[derive(Debug, PartialEq, Eq, FromXml, ToXml)]
+#[xml(ns("URI"))]
+struct VecEntities<'a> {
+    complex: Vec<StructSpecialEntities<'a>>,
+    list1: Vec<String>,
+    list2: Vec<Vec<String>>,
+}
+
+#[test]
+fn vec_entities() {
+    let serialized = r#"<VecEntities xmlns="URI"><complex><StructSpecialEntities><string>&lt;&gt;&amp;&quot;&apos;adsad&quot;</string><str>str</str><cow>str&amp;</cow><vec><element xmlns="">one</element><element xmlns="">two</element><element xmlns="">three</element></vec></StructSpecialEntities></complex><list1><element xmlns="">a</element><element xmlns="">b</element></list1><list2><list xmlns=""><element xmlns="">a</element><element xmlns="">b</element></list></list2></VecEntities>"#;
+
+    let instance = VecEntities {
+        complex: vec![StructSpecialEntities {
+            string: String::from("<>&\"'adsad\""),
+            str: "str",
+            cow: Cow::Owned("str&".to_string()),
+            vec: vec!["one".into(), "two".into(), "three".into()],
+        }],
+        list1: vec!["a".into(), "b".into()],
+        list2: vec![vec!["a".into(), "b".into()]],
+    };
+
+    assert_eq!(to_string(&instance).unwrap(), serialized);
+    assert_eq!(from_str(serialized), Ok(instance));
+}
+
 #[test]
 fn escape_back() {
     assert_eq!(

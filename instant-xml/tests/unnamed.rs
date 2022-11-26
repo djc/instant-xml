f39beb1485cc7dbb9b8fe98c626b8c3ff6f81c27
Dirkjan Ochtman <dirkjan@ochtman.nl>
2022-11-26 14:57:12 -0800

    Add support for elements in unnamed fields

--- instant-xml/tests/unnamed.rs
+++ instant-xml/tests/unnamed.rs
@@ -10,3 +10,28 @@ fn one_number() {
     assert_eq!(xml, to_string(&v).unwrap());
     assert_eq!(v, from_str(xml).unwrap());
 }
+
+#[derive(Debug, Eq, FromXml, PartialEq, ToXml)]
+struct OneString(String);
+
+#[test]
+fn one_string() {
+    let v = OneString("f42".to_owned());
+    let xml = r#"<OneString>f42</OneString>"#;
+    assert_eq!(xml, to_string(&v).unwrap());
+    assert_eq!(v, from_str(xml).unwrap());
+}
+
+#[derive(Debug, Eq, FromXml, PartialEq, ToXml)]
+struct StringElement(String, Foo);
+
+#[derive(Debug, Eq, FromXml, PartialEq, ToXml)]
+struct Foo;
+
+#[test]
+fn string_element() {
+    let v = StringElement("f42".to_owned(), Foo);
+    let xml = r#"<StringElement>f42<Foo></Foo></StringElement>"#;
+    assert_eq!(xml, to_string(&v).unwrap());
+    assert_eq!(v, from_str(xml).unwrap());
+}

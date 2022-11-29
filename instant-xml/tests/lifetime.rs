d2605977aa4c1e535807a3382432467ba62f0d5c
Dirkjan Ochtman <dirkjan@ochtman.nl>
2022-11-29 13:07:41 +0100

    Add some extra borrowing tests

---
+++ instant-xml/tests/lifetime.rs
@@ -0,0 +1,27 @@
+use std::borrow::Cow;
+
+use similar_asserts::assert_eq;
+
+use instant_xml::{from_str, to_string, FromXml, ToXml};
+
+#[derive(Debug, Eq, FromXml, PartialEq, ToXml)]
+struct Foo {
+    bar: Bar<'static>,
+}
+
+#[derive(Debug, Eq, FromXml, PartialEq, ToXml)]
+struct Bar<'a> {
+    baz: Cow<'a, str>,
+}
+
+#[test]
+fn lifetime() {
+    let v = Foo {
+        bar: Bar {
+            baz: Cow::Borrowed("hello"),
+        },
+    };
+    let xml = r#"<Foo><Bar><baz>hello</baz></Bar></Foo>"#;
+    assert_eq!(xml, to_string(&v).unwrap());
+    assert_eq!(v, from_str(xml).unwrap());
+}

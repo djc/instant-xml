786f035f765055c57ecbeee442cbd7ae388e4964
Dirkjan Ochtman <dirkjan@ochtman.nl>
2023-02-22 17:23:19 +0100

    Implement transparent mode for deserialization

--- instant-xml/tests/transparent.rs
+++ instant-xml/tests/transparent.rs
@@ -1,23 +1,23 @@
-use instant_xml::{to_string, ToXml};
+use instant_xml::{from_str, to_string, FromXml, ToXml};
 
-#[derive(Debug, Eq, PartialEq, ToXml)]
+#[derive(Debug, Eq, FromXml, PartialEq, ToXml)]
 struct Wrapper {
     inline: Inline,
 }
 
-#[derive(Debug, Eq, PartialEq, ToXml)]
+#[derive(Debug, Eq, FromXml, PartialEq, ToXml)]
 #[xml(transparent)]
 struct Inline {
     foo: Foo,
     bar: Bar,
 }
 
-#[derive(Debug, Eq, PartialEq, ToXml)]
+#[derive(Debug, Eq, FromXml, PartialEq, ToXml)]
 struct Foo {
     i: u8,
 }
 
-#[derive(Debug, Eq, PartialEq, ToXml)]
+#[derive(Debug, Eq, FromXml, PartialEq, ToXml)]
 struct Bar {
     s: String,
 }
@@ -32,6 +32,8 @@ fn inline() {
             },
         },
     };
+
     let xml = r#"<Wrapper><Foo><i>42</i></Foo><Bar><s>hello</s></Bar></Wrapper>"#;
     assert_eq!(xml, to_string(&v).unwrap());
+    assert_eq!(v, from_str(xml).unwrap());
 }

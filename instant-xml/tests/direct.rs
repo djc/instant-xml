f0e815a9ea54c08879d6d38a7b84c31d934cd613
Nicholas Rempel <nbrempel@gmail.com>
2024-04-06 14:23:57 -0700

    Add support for comments

--- instant-xml/tests/direct.rs
+++ instant-xml/tests/direct.rs
@@ -2,7 +2,7 @@ use similar_asserts::assert_eq;
 
 use instant_xml::{from_str, to_string, FromXml, ToXml};
 
-#[derive(Debug, Eq, FromXml, PartialEq, ToXml)]
+#[derive(Clone, Debug, Eq, FromXml, PartialEq, ToXml)]
 struct Foo {
     #[xml(attribute)]
     flag: bool,
@@ -19,5 +19,14 @@ fn direct() {
     let xml = "<Foo flag=\"true\">cbdté</Foo>";
 
     assert_eq!(to_string(&v).unwrap(), xml);
+    assert_eq!(from_str::<Foo>(xml), Ok(v.clone()));
+
+    let xml = "<Foo flag=\"true\"><!--comment-->cbdté</Foo>";
+    assert_eq!(from_str::<Foo>(xml), Ok(v.clone()));
+
+    let xml = "<Foo flag=\"true\"><!--comment--><!--comment-->cbdté</Foo>";
+    assert_eq!(from_str::<Foo>(xml), Ok(v.clone()));
+
+    let xml = "<!--comment--><Foo flag=\"true\"><!--comment-->cbdté</Foo><!--comment-->";
     assert_eq!(from_str::<Foo>(xml), Ok(v));
 }

a1fed6bbb0c0ec05a6c37028b47f6c80061d2c02
Stuart Bruce <programming@brucey.me.uk>
2025-01-30 12:03:25 +0000

    Remove test type aliases

--- instant-xml/tests/chrono.rs
+++ instant-xml/tests/chrono.rs
@@ -10,21 +10,17 @@ struct Test<T> {
     dt: T,
 }
 
-type TestUtcDateTime = Test<DateTime<Utc>>;
-
 #[test]
 fn datetime() {
     let dt = Utc.with_ymd_and_hms(2022, 11, 21, 21, 17, 23).unwrap();
     let test = Test { dt };
     let xml = "<Test><dt>2022-11-21T21:17:23+00:00</dt></Test>";
     assert_eq!(to_string(&test).unwrap(), xml);
-    assert_eq!(from_str::<TestUtcDateTime>(xml).unwrap(), test);
+    assert_eq!(from_str::<Test<DateTime<Utc>>>(xml).unwrap(), test);
 
     let zulu = xml.replace("+00:00", "Z");
-    assert_eq!(from_str::<TestUtcDateTime>(&zulu).unwrap(), test);
+    assert_eq!(from_str::<Test<DateTime<Utc>>>(&zulu).unwrap(), test);
 }
-
-type TestNaiveDateTime = Test<NaiveDateTime>;
 
 #[test]
 fn naive_datetime() {
@@ -32,5 +28,5 @@ fn naive_datetime() {
     let test = Test { dt };
     let xml = "<Test><dt>2022-11-21T21:17:23</dt></Test>";
     assert_eq!(to_string(&test).unwrap(), xml);
-    assert_eq!(from_str::<TestNaiveDateTime>(xml).unwrap(), test);
+    assert_eq!(from_str::<Test<NaiveDateTime>>(xml).unwrap(), test);
 }

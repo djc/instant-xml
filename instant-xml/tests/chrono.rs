27a0b56c712f5187c7bb04dbf742dd01dc953891
Stuart <SirBrucey@users.noreply.github.com>
2025-02-11 12:52:46 +0000

    Impl (de)serialization for chrono::NaiveDateTime (#87)

--- instant-xml/tests/chrono.rs
+++ instant-xml/tests/chrono.rs
@@ -1,13 +1,13 @@
 #![cfg(feature = "chrono")]
 
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
 use similar_asserts::assert_eq;
 
 use instant_xml::{from_str, to_string, FromXml, ToXml};
 
 #[derive(Debug, Eq, PartialEq, FromXml, ToXml)]
-struct Test {
-    dt: DateTime<Utc>,
+struct Test<T> {
+    dt: T,
 }
 
 #[test]
@@ -16,8 +16,17 @@ fn datetime() {
     let test = Test { dt };
     let xml = "<Test><dt>2022-11-21T21:17:23+00:00</dt></Test>";
     assert_eq!(to_string(&test).unwrap(), xml);
-    assert_eq!(from_str::<Test>(xml).unwrap(), test);
+    assert_eq!(from_str::<Test<DateTime<Utc>>>(xml).unwrap(), test);
 
     let zulu = xml.replace("+00:00", "Z");
-    assert_eq!(from_str::<Test>(&zulu).unwrap(), test);
+    assert_eq!(from_str::<Test<DateTime<Utc>>>(&zulu).unwrap(), test);
+}
+
+#[test]
+fn naive_datetime() {
+    let dt = NaiveDateTime::parse_from_str("2022-11-21T21:17:23", "%Y-%m-%dT%H:%M:%S").unwrap();
+    let test = Test { dt };
+    let xml = "<Test><dt>2022-11-21T21:17:23</dt></Test>";
+    assert_eq!(to_string(&test).unwrap(), xml);
+    assert_eq!(from_str::<Test<NaiveDateTime>>(xml).unwrap(), test);
 }
